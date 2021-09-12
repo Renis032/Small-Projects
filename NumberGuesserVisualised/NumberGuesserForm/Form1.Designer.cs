ecb4bc2783a824072b7d07a5797cf3a560fbce74
Renis032 <renismyumyun@gmail.com>
2021-09-12 18:59:32 +0200

    Visualised version of NumberGuess

---
+++ NumberGuesserVisualised/NumberGuesserForm/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿
+namespace NumberGuesserForm
+{
+    partial class Tries
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GuessButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.NumberLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.RestartButton = new System.Windows.Forms.Button();
+            this.SmallerGreater = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // GuessButton
+            // 
+            this.GuessButton.Font = new System.Drawing.Font("Microsoft YaHei", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.GuessButton.Location = new System.Drawing.Point(135, 293);
+            this.GuessButton.Name = "GuessButton";
+            this.GuessButton.Size = new System.Drawing.Size(160, 51);
+            this.GuessButton.TabIndex = 0;
+            this.GuessButton.Text = "Guess";
+            this.GuessButton.UseVisualStyleBackColor = true;
+            this.GuessButton.Click += new System.EventHandler(this.GuessButton_Click_1);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(135, 217);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(160, 28);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // NumberLabel
+            // 
+            this.NumberLabel.Font = new System.Drawing.Font("Microsoft YaHei", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.NumberLabel.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.NumberLabel.Location = new System.Drawing.Point(51, 18);
+            this.NumberLabel.Name = "NumberLabel";
+            this.NumberLabel.Size = new System.Drawing.Size(339, 44);
+            this.NumberLabel.TabIndex = 2;
+            this.NumberLabel.Text = "Guess the number!";
+            this.NumberLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label1.Location = new System.Drawing.Point(313, 373);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 21);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Tries left: ";
+            // 
+            // RestartButton
+            // 
+            this.RestartButton.Font = new System.Drawing.Font("Microsoft YaHei", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.RestartButton.Location = new System.Drawing.Point(135, 293);
+            this.RestartButton.Name = "RestartButton";
+            this.RestartButton.Size = new System.Drawing.Size(160, 51);
+            this.RestartButton.TabIndex = 4;
+            this.RestartButton.Text = "Restart";
+            this.RestartButton.UseVisualStyleBackColor = true;
+            this.RestartButton.Visible = false;
+            this.RestartButton.Click += new System.EventHandler(this.RestartButton_Click);
+            // 
+            // SmallerGreater
+            // 
+            this.SmallerGreater.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.SmallerGreater.Location = new System.Drawing.Point(104, 62);
+            this.SmallerGreater.Name = "SmallerGreater";
+            this.SmallerGreater.Size = new System.Drawing.Size(216, 71);
+            this.SmallerGreater.TabIndex = 5;
+            this.SmallerGreater.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label2.Location = new System.Drawing.Point(12, 373);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(124, 21);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "0-20";
+            // 
+            // Tries
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.CausesValidation = false;
+            this.ClientSize = new System.Drawing.Size(432, 403);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.SmallerGreater);
+            this.Controls.Add(this.RestartButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.NumberLabel);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.GuessButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Tries";
+            this.Text = "NumberGuesser";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button GuessButton;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label NumberLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button RestartButton;
+        private System.Windows.Forms.Label SmallerGreater;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ NumberGuesserVisualised/NumberGuesserForm/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NumberGuesserForm
+{
+    public partial class Tries : Form
+    {
+        public static Random rnd = new Random();
+        int tries = 5;
+        string guess;
+        static int numToGuess = rnd.Next(0, 21);
+
+        public Tries()
+        {
+            InitializeComponent();
+            this.AcceptButton = GuessButton;
+            label1.Text = "Tries left: " + tries;
+        }
+
+        private void checkWinner(String userGuess)
+        {
+            try
+            {
+                NumberLabel.Text = "Guess the number!";
+                int guessedNum = Int32.Parse(guess);
+                SmallerGreater.Visible = true;
+
+                if (guessedNum == numToGuess)
+                {
+                    SmallerGreater.Text = "You Win!";
+                    GuessButton.Visible = false;
+                    RestartButton.Visible = true;
+                    GuessButton.Enabled = false;
+                }
+                else if (guessedNum > numToGuess)
+                {
+                    SmallerGreater.Text = "Lower!";
+                    tries--;
+                    label1.Text = "Tries left: " + tries;
+                }
+                else if (guessedNum < numToGuess)
+                {
+                    SmallerGreater.Text = "Greater!";
+                    tries--;
+                    label1.Text = "Tries left: " + tries;
+                }
+              
+                if (tries == 0)
+                {
+                    SmallerGreater.Text = "You lose :( The number was " + numToGuess;
+                    GuessButton.Visible = false;
+                    RestartButton.Visible = true;
+                    GuessButton.Enabled = false;
+                }
+            }
+            catch (Exception e)
+            {
+                NumberLabel.Text = "Error";
+                textBox1.Text = "";
+                SmallerGreater.Text = "";
+            }
+        }
+
+        private void GuessButton_Click_1(object sender, EventArgs e)
+        {
+            guess = textBox1.Text;
+            checkWinner(guess);
+        }
+
+        private void RestartButton_Click(object sender, EventArgs e)
+        {
+            GuessButton.Enabled = true;
+            RestartButton.Visible = false;
+            GuessButton.Visible = true;
+            textBox1.Text = "";
+            tries = 6;
+            numToGuess = rnd.Next(0, 21);
+            SmallerGreater.Text = "";
+            SmallerGreater.Visible = false;
+            checkWinner(guess);
+        }
+    }
+}
